9d290643d2989b2c6926cfe821f05c49891c11c9
Maksim Hancharevich <maxmegapixel@gmail.com>
2023-09-24 00:52:41 +0300

    task 1

--- Program.cs
+++ Program.cs
@@ -8,3 +8,32 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] massive = GetArray();
+Console.WriteLine($"Массив: [{String.Join(", ", massive)}]");
+Console.WriteLine($"Новый: [{String.Join(", ", GetNew(massive))}]");
+
+string[] GetArray()  // метод для ввода массива
+{
+    Console.Write("Введите количество элементов массива: ");
+    int number = int.Parse(Console.ReadLine()!);
+    string[] result = new string[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        result[i] = Console.ReadLine()!;
+    }
+    return result;
+}
+
+// метод для формирования нового массива
+string[] GetNew(string[] array)
+{
+    string temp = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) temp += array[i]+" "; 
+    }
+    string[] result = temp.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    return result;
+}
+
